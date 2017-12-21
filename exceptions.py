b606e2f17380c8d2212ad8051ab5c021e241d8f7
Rivosyke <rcmcginn82@gmail.com>
2017-12-21 15:59:10 +0100

    exceptions commit

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python
 
+try:
+	a = 0/1
+except:
+	print "Exception: Divide by zero"
+else:
+	print "No exception"
+finally:
+	print "Cleanup code"
+
+
+try:
+	a = 0/0
+except ZeroDivisionError:
+	print "Divide by zero"
+except:
+	print "Unknown error"
+	
+
+try:
+	a = 0/0
+except Exception as im:
+	print (dir(im))
+	
