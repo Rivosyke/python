39a1730ee85bad6961b041ad466c61b66fb9bd49
Rivosyke <rcmcginn82@gmail.com>
2017-12-29 16:58:39 +0100

    29 DEC 2017 Commit (From Work #2)

--- cs_programming.py
+++ cs_programming.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 
 import socket
-#                            Address Family   Reliability Option (TCP)
+# Address Family   Reliability Option (TCP)
 tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+# Interface to listen on, port number (as a tuple)
 tcpSocket.bind(("0.0.0.0", 8000))
+
+# Max number of concurrent connections
+tcp.Socket.listen(2)
+
+
+
