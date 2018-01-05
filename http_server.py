041787ae077f75b9548c2df80c3c4a8187e28ad0
Rivosyke <rcmcginn82@gmail.com>
2018-01-05 15:41:53 +0100

    05-JAN-2018

---
+++ http_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import SocketServer
+import SimpleHTTPServer
+
+
+
+class HttpRequestHandler (SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		if self.path == '/admin':
+			self.wfile.write('Only for admins!')
+			self.wfile.write(self.headers)
+		else:
+			SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpServer = SocketServer.TCPServer(('', 5000), HttpRequestHandler)
+
+httpServer.serve_forever()
