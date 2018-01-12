6a0752600c77744f019e10f33662b4e98241ee93
Rivosyke <rcmcginn82@gmail.com>
2018-01-12 16:03:23 +0100

    12-JAAN-2018

--- module_3/raw_inject.py
+++ module_3/raw_inject.py
@@ -8,10 +8,10 @@ import struct
 ARP_packet = 0x0806
 IP_packet = 0x0800
 rawSocket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(ARP_packet))
+rawSocket.bind(("ens33", socket.htons(ARP_packet)))
 
 ethr_src = '\xfe\x80\x34\x92\x89\x7f'
 ethr_dst = '\x00\x50\x56\xC0\x00\x08'
-
 ethr_typ = '\x08\x06'
 
 arp_hw_typ = '\x00\x01'
@@ -26,15 +26,10 @@ arp_ip_dst = '\xc0\xa8\x6f\x45'
 
 ethr_frame_pack = '!6s6s2s'
 arp_frame_pack = '!2s2sss2s6s4s6s4s'
-full_frame_pack = (ethr_frame_pack + arp_frame_pack)
-
-
-rawSocket.bind(("ens33", socket.htons(ARP_packet)))
+#full_frame_pack = (ethr_frame_pack + arp_frame_pack)
 
 ethr_frame = struct.pack(ethr_frame_pack, ethr_dst, ethr_src, ethr_typ)
-arp_frame = struct.pack(full_frame_pack, arp_hw_typ, arp_proto_typ, 
-						arp_hw_len, arp_proto_len, arp_opcode,
-						arp_eth_src, arp_ip_src, arp_eth_dst, arp_ip_dst)
+arp_frame = struct.pack(arp_frame_pack, arp_hw_typ, arp_proto_typ, arp_hw_len, arp_proto_len, arp_opcode, arp_eth_src, arp_ip_src, arp_eth_dst, arp_ip_dst)
 
 packet = ethr_frame + arp_frame
 
