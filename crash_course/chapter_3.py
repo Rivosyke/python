0a7bb5d3873e4d33f25fb41b49341d4cc4eb9d0b
Rivosyke <rcmcginn82@gmail.com>
2018-01-26 14:57:07 +0100

    blah 26-JAN-2018

---
+++ crash_course/chapter_3.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+listA = ['Dubai', 'London', 'Andorra', 'Japan', 'Mutrah']
+
+print listA
+
+print sorted(listA)
+
+print listA
+
+listA.reverse()
+
+print listA
+
+listA.reverse()
+
+print listA
+
+listA.sort()
+
+print listA
+
+listA.reverse()
+
+print listA
+
+
+try:
+	print listA[10]
+except:
+	print "You done goofed!"
+ 
+	
---
+++ crash_course/chapter_4.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli', 'bob', 'kevin', 'lyle', 'brad']
+
+
+
