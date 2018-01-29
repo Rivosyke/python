c0b3d8d5dcdf3f616e50e73d9a712faba09b0d17
Rivosyke <rcmcginn82@gmail.com>
2018-01-29 17:06:58 +0100

    blah 29/01/2018

--- crash_course/chapter_6.py
+++ crash_course/chapter_6.py
@@ -8,7 +8,18 @@ player_status = {}
 
 for player in players:
 	player_status[player] = 'dead'
-	
+
+for i, player in enumerate(player_status):
+	if i % 2 == 0:
+		player_status[player] = 'alive'
+		
 for player in player_status:
 	print "Player: " + player
 	print "Status: " + player_status[player]
+
+
+email_recipient = {}
+
+email_recipient['first_name'] = 'bob'
+email_recipient['last_name'] = 'sanker'
+email_recipient['md5_hash'] = '1A2B3C'
